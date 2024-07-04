559dec281c1fffe5d0cd66627ea4dbdea04d557f
aila8913 <aila8913@gmail.com>
2024-07-04 15:43:15 +0800

    fcntl

--- Amazon/setup.py
+++ Amazon/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="AmazonProject",  # 專案名稱
-    version="0.1",  # 專案版本
+    version="1.0",  # 專案版本
     packages=find_packages(),  # 自動尋找所有包
     install_requires=[  # 列出專案的依賴
         'Flask',
